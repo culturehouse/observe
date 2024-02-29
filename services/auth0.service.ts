edc31f9a203469b9d369657935afc192446d3b44
ying13 <ying@rently.com>
2024-02-29 11:56:16 -0500

    remove auth0 audience

--- services/auth0.service.ts
+++ services/auth0.service.ts
@@ -4,5 +4,4 @@ export const auth = new auth0.WebAuth({
   domain: `${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}`,
   clientID: `${process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}`,
   scope: `${process.env.NEXT_PUBLIC_AUTH0_USER_SCOPE}`,
-  audience: "https://culture.house/",
 });
